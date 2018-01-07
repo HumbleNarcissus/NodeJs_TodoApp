6578236e9223442e7a6ef6d439f27daa8986f077
HumbleNarcissus <maciek.tarach06@gmail.com>
2018-01-07 21:31:16 +0100

    Setting up diffrent environments

--- server/app.js
+++ server/app.js
@@ -1,3 +1,14 @@
+let env = process.env.NODE_ENV || 'develompent';
+
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/Note';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/NoteTest';
+}
+
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -6,7 +17,7 @@ let {mongoose} = require('./db/mongo');
 let {User} = require('./models/user');
 let {Todo} = require('./models/todo');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 //express app
 let app = express();
@@ -56,6 +67,52 @@ app.get('/todos/:id', (req, res) => {
         res.status(400).send();
     });
 });
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send(todo);
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
+    })
+
+});
+
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
