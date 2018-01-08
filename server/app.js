c2065c1b0816049866c2a5e3c286d7b104e993e4
HumbleNarcissus <maciek.tarach06@gmail.com>
2018-01-08 22:24:39 +0100

    user model and users path

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-let env = process.env.NODE_ENV || 'develompent';
+let env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
     process.env.PORT = 3000;
@@ -113,6 +113,19 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+  
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      res.header('x-auth', token).send(user);
+    }).catch((err) => {
+      res.status(400).send(err);
+    })
+  });
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
